1f7274ca91dc0bf8fdcf632e9cbc252e7289b320
ReneRojasR <101284294+ReneRojasR@users.noreply.github.com>
2022-08-20 21:17:38 -0400

    Update aplicando contenidos 2
    aplicamos controles

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  usuario: string = "Desconocido";
+  clave: string = "";
+
   constructor() {}
 
+  
+  item: any = {
+    imagen:"",
+    icono:"",
+  }
 }
