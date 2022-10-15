80e348226cad5069f4fb0a31f0ec5af79e5c9bbf
ReneRojasR <101284294+ReneRojasR@users.noreply.github.com>
2022-10-14 22:39:14 -0300

    Cambios a GeoLocalizacion
    Sigue con varios errores que no comprendo del todo, falta la api de google map que no entiendo como instalarla la verdad.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,38 @@ import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(protected platform: Platform, protected navController: NavController, public localStorage: NativeStorage ) 
+  constructor(protected platform: Platform, protected navController: NavController, public localStorage: NativeStorage,
+  private geolocation: Geolocation) 
   {
     this.platform.ready().then(async () => {
-      await this.localStorage.setItem;
+      await this.localStorage.getItem;
+      this.getGeolocation();
     });
   }
 
+  getGeolocation(){
 
-  
+    this.geolocation.getCurrentPosition().then((resp) => {
+    
+      console.log("resp", resp);
+
+      this.GeolService.locations[0].geometry.coordinates =[
+        resp.coords.latitude,
+        resp.coords.longitude
+    ];
+
+    
+    }).catch((error) => {
+     console.log('Error getting location', error);
+    });
+     
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+    });
+  }
 }
 
 
--- src/app/services/API/geol.service.ts
+++ src/app/services/API/geol.service.ts
@@ -1,16 +1,17 @@
+import { LocationStrategy } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { Geolocation} from '@awesome-cordova-plugins/geolocation/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
+
+locations = [];
+
 export class GeolService {
 
   constructor() {
     
   }
-
-  
-
   
 }
